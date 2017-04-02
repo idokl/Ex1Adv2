51d0312e0fecc2c0b40ef052e695f9ab2ecf9ce1
ido <idoklein1@gmail.com>
2017-04-02 21:03:49 +0300

    I copied the searcher implementation from AP2 presentation

---
+++ searchalgorithmslib/searcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    public abstract class Searcher: ISearcher
+    {
+        private PriorityQueue<State> openList;
+        private int evaluatedNodes;
+        
+        public Searcher()
+        {
+            openList = new PriorityQueue<State>();
+            evaluatedNodes = 0;
+        }
+
+        protected State popOpenList()
+        {
+            evaluatedNodes++;
+            return openList.pop();
+        }
+        // a property of openList
+
+        public int OpenListSize
+        { // it is a read-only property :)
+            get { return openList.Count; }
+        }
+        
+        // ISearcher’s methods:
+        public int getNumberOfNodesEvaluated()
+        {
+            return evaluatedNodes;
+        }
+        public abstract Solution search(ISearchable searchable);
+    }
+}

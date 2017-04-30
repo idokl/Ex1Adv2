2b20f9cb0e493b59aeca63cc7af725a64c15135d
ohad cohen <ohadc7@gmail.com>
2017-04-30 14:28:51 +0300

    add mvvm cunstruction

---
+++ GUI/Model/SettingsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Model
+{
+    class SettingsModel : ISettingsModel
+    {
+        public string ServerIP
+        {
+            get { return Properties.Settings.Default.ServerIP; }
+            set { Properties.Settings.Default.ServerIP = value; }
+        }
+        public int ServerPort
+        {
+            get { return Properties.Settings.Default.ServerPort; }
+            set { Properties.Settings.Default.ServerPort = value; }
+        }
+    
+        public int MazeRows { get; set; }
+        public int MazeCols { get; set; }
+        public int SearchAlgorithm { get; set; }
+
+        public void SaveSettings()
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ GUI/View/SettingsWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using GUI.ViewModel;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        private SettingsViewModel vm;
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            vm = new SettingsViewModel();
+            this.DataContext = vm;
+        }
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            vm.SaveSettings();
+            MainWindow win = (MainWindow)Application.Current.MainWindow;
+            win.Show();
+            this.Close();
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = (MainWindow)Application.Current.MainWindow;
+            win.Show();
+            this.Close();
+        }
+    }
+}

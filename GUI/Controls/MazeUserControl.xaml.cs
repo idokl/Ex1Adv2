763ded2f9c45d9e93273e1fc9adf7044f840d920
ohadc7 <ohadc7@gmail.com>
2017-05-14 02:29:02 +0300

    draw maze with user control works!

--- GUI/Controls/MazeUserControl.xaml.cs
+++ GUI/Controls/MazeUserControl.xaml.cs
@@ -27,8 +27,7 @@ namespace GUI.Controls
     public partial class MazeUserControl : UserControl
     {
         //  private MazeUserControlViewModel mazeUserControlViewModel;
-        private ObservableCollection<RectItem> RectItems = new ObservableCollection<RectItem>();
-        private ObservableCollection<Rectangle> Rectangles = new ObservableCollection<Rectangle>();
+        private ObservableCollection<Path> Rectangles = new ObservableCollection<Path>();
 
         public MazeUserControl()
         {
@@ -36,99 +35,54 @@ namespace GUI.Controls
 
 
             var dfsMazeGenerator = new DFSMazeGenerator();
-            var MyMaze = dfsMazeGenerator.Generate(20, 20);
-            //MyMaze.Name;
+            var MyMaze = dfsMazeGenerator.Generate(15,15);
             var SM = new SearchableMaze(MyMaze);
+           PointState startPoint =  new PointState(MyMaze.InitialPos);
+            PointState endPoint = new PointState(MyMaze.GoalPos);
 
-            int size = MyMaze.Rows * MyMaze.Cols;
-            Rectangle rect = new Rectangle();
-          
+
              int widthOfBlock = 40;
             int heightOfBlock = 40;
-           // double height = 100;//System.Windows.SystemParameters.PrimaryScreenHeight;
-            //double width = 100;//System.Windows.SystemParameters.PrimaryScreenWidth;
-           // int widthOfBlock = (int)width / MyMaze.Cols;
-            //int heightOfBlock = (int)height / MyMaze.Rows;
+            
             for (int i = 0; i < MyMaze.Rows; i++)
-            {
+             {
                 for (int j = 0; j < MyMaze.Cols; j++)
                 {
-                    Rectangle rec = new Rectangle();
-                    rec.Width = widthOfBlock;
-                    rec.Height = heightOfBlock;
+
+                    var rec = new Path
+                    {
+                        Data = new RectangleGeometry(new Rect(j * widthOfBlock, i * heightOfBlock, widthOfBlock, heightOfBlock)),
+                        Fill = Brushes.Black,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 2
+                    };
+
                     if (CellType.Free == MyMaze[i, j])
                     {
-                        rec.Fill = new SolidColorBrush(System.Windows.Media.Colors.White);
-                        rec.Stroke = new SolidColorBrush(System.Windows.Media.Colors.Black);
+                        rec.Fill = Brushes.White;
+
+
                     }
-                    else
+                    if(new Position(i,j).Equals(MyMaze.InitialPos))
                     {
-                        rec.Fill = new SolidColorBrush(System.Windows.Media.Colors.Black);
+                        rec.Fill = Brushes.Blue;
                     }
-                    
-                    rec.RadiusX = j * widthOfBlock;
-                    rec.RadiusY = i * heightOfBlock;
-                    this.Rectangles.Add(rec);
+                    if (new Position(i, j).Equals(MyMaze.GoalPos))
+                    {
+                        rec.Fill = Brushes.Red;
+                    }
+                    MyCanvas.Children.Add(rec);
+
 
                 }
-            }
-            /*
-                    for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Rectangle rec = new Rectangle();
+             }
+            
+         
+            //icRectangles.ItemsSource = Rectangles;
 
-                    
-                    rec.Width = widthOfBlock;
-                    rec.Height = heightOfBlock;
-                    rec.Fill = new SolidColorBrush(System.Windows.Media.Colors.Black);
-                    //rec.X = j * widthOfBlock;
-                    //rec.Y = i * heightOfBlock;
-
-
-                    this.Rectangles.Add(rec);
-                }
-            }*/
-            icRectangles.ItemsSource = Rectangles;
-
-
-            //this.draw();
             this.DataContext = this;
         }
 
-        public void draw()
-        {
-            var dfsMazeGenerator = new DFSMazeGenerator();
-            var MyMaze = dfsMazeGenerator.Generate(5, 5);
-            //MyMaze.Name;
-            var SM = new SearchableMaze(MyMaze);
-            int size = MyMaze.Rows * MyMaze.Cols;
-            //this.RectItems = new ObservableCollection<RectItem>();
-
-            double height = System.Windows.SystemParameters.PrimaryScreenHeight;
-            double width = System.Windows.SystemParameters.PrimaryScreenWidth;
-            int widthOfBlock = (int) width / MyMaze.Cols;
-            int heightOfBlock = (int) height / MyMaze.Rows;
-            for (int i = 0; i < MyMaze.Rows; i++)
-            {
-                for (int j = 0; j < MyMaze.Cols; j++)
-                {
-                    RectItem rec = new RectItem();
-                    
-                    if (CellType.Free == MyMaze[i, j])
-                    {
-                        rec.WhiteColor = true;
-                    }
-                    rec.Width = widthOfBlock;
-                    rec.Height = heightOfBlock;
-                    rec.X = j * widthOfBlock;
-                    rec.Y = i * heightOfBlock;
-                  
-
-                    this.RectItems.Add(rec);
-                }
-            }
-        }
+      
     }
 }
